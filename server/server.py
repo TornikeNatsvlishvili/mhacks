5626ffaa4ce22a7abf85f87acdc7bbe14ca50f7e
gokusper <endriasred@gmail.com>
2016-02-20 13:08:10 -0500

    search methods added

--- server/server.py
+++ server/server.py
@@ -13,12 +13,30 @@ def root():
         'pos-tag': article.pos_tag()})
 
 @app.route("/picture/<query>")
-def picture(query):
-    pass
+def search_image(name):
+    query = urllib.parse.urlencode({'q': name})
+    result = read_html('http://images.google.com/images?'+query+'&sout=1')
+    soup = BeautifulSoup(result,'html.parser')
+    ps = soup.find_all("img")
+    try:
+       value = ps[0].get('src')
+       return value
+    except Exception as e:
+        output = format(e)
+        print(output)
 
 @app.route("/search/<query>")
-def search(query):
-    pass
+def search(name):
+    query = urllib.parse.urlencode({'q': name})
+    result = read_html('https://www.google.com/search?'+query)
+    soup = BeautifulSoup(result,'html.parser')
+    ps = soup.find_all("a")
+    try:
+       value = ps[0].get('data-href')
+       return value
+    except Exception as e:
+        output = format(e)
+        print(output)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
